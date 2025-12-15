14aa50a58d37d25127cbce9b52d8da976a5fcc8e
exercio-java-poo <victorbaraldi@gmail.com>
2025-12-15 16:29:00 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourcesExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exception.DatabaseException;
 import com.educandoweb.course.services.exception.ResourcesNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,5 +22,12 @@ public class ResourcesExceptionHandler {
 		StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandarError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
